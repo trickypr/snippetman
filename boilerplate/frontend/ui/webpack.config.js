3f5b3dd435b0a832f8464694e0f448f1d1c13e5b
trickypr <trickypr@icloud.com>
2022-08-13 22:19:56 +1000

    ⚡️ Heavily optimize final builds

--- boilerplate/frontend/ui/webpack.config.js
+++ boilerplate/frontend/ui/webpack.config.js
@@ -7,75 +7,87 @@ import TSReactRefresh from "react-refresh-typescript";
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-export default {
-  entry: "./src/main.tsx",
-  output: {
-    path: resolve(__dirname, "dist"),
-    filename: "[name].js",
-    clean: true,
-  },
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    // static: "./dist",
-    hot: true,
-    allowedHosts: ["all"],
-    devMiddleware: {
-      writeToDisk: true,
+export default (emv, argv) => {
+  const dev = argv.mode === "development";
+  const prod = argv.mode === "production";
+
+  return {
+    entry: "./src/main.tsx",
+    output: {
+      path: resolve(__dirname, "dist"),
+      filename: "[name].js",
+      clean: true,
     },
-    client: {
-      logging: "verbose",
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: {
-          getCustomTransformers: () => ({
-            before: [TSReactRefresh()],
-          }),
-          transpileOnly: true,
-        },
+    devtool: dev ? "inline-source-map" : "source-map",
+    devServer: {
+      hot: true,
+      allowedHosts: ["all"],
+      devMiddleware: {
+        writeToDisk: true,
       },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
+      client: {
+        logging: "verbose",
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          options: {
+            getCustomTransformers: () => ({
+              before: [dev && TSReactRefresh()].filter(Boolean),
+            }),
+            transpileOnly: true,
           },
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-        exclude: /\.module\.css$/,
-      },
-    ],
-  },
-  plugins: [
-    new ReactRefreshPlugin({
-      overlay: {
-        sockPort: 8080,
-        sockHost: "localhost",
-        sockProtocol: "http",
-      },
-    }),
-    new CopyPlugin({
-      patterns: [{ from: "public" }],
-    }),
-  ],
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  experiments: {
-    topLevelAwait: true,
-  },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                modules: true,
+              },
+            },
+          ],
+          include: /\.module\.css$/,
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+          exclude: /\.module\.css$/,
+        },
+      ],
+    },
+    plugins: [
+      dev &&
+        new ReactRefreshPlugin({
+          overlay: {
+            sockPort: 8080,
+            sockHost: "localhost",
+            sockProtocol: "http",
+          },
+        }),
+      new CopyPlugin({
+        patterns: [{ from: "public" }],
+      }),
+    ].filter(Boolean),
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    experiments: {
+      topLevelAwait: true,
+    },
+
+    optimization: prod
+      ? {
+          usedExports: true,
+        }
+      : {
+          /* Dev is optimized */
+        },
+  };
 };
