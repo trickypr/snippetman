62824a98b11196b2bcd80d4ccea9a8a29b9a94b1
trickypr <trickypr@icloud.com>
2022-08-14 20:50:10 +1000

    ⚡️ Minimize memory regularly

--- boilerplate/frontend/ui/src/main.tsx
+++ boilerplate/frontend/ui/src/main.tsx
@@ -6,3 +6,13 @@ import App from "./App";
 import "./index.css";
 
 render(<App />, document.getElementById("root") as HTMLElement);
+
+let gMgr = Cc["@mozilla.org/memory-reporter-manager;1"].getService(
+  Ci.nsIMemoryReporterManager
+);
+
+// Minimize memory usage once a minute. This might need to be removed in the
+// future if it causes performance issues.
+setInterval(() => {
+  gMgr.minimizeMemoryUsage(() => {});
+}, 1000 * 60);
