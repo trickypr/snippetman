d3ce57c21b418f31bad1a89303b857be3f467bac
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-07-16 19:09:35 +1000

    ✨ Make search functional

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Fuse from 'fuse.js'
+
   import Sidebar from './components/sidebar/Sidebar.svelte'
   import SnippetCard from './components/SnippetCard.svelte'
   import Snippet from './components/Snippet.svelte'
@@ -9,15 +11,31 @@
     snippets,
     tagFilters,
   } from './store/appState'
-  import { createSnippet } from './store/snippets'
+  import { createSnippet, type TaggedSnippet } from './store/snippets'
 
-  $: filteredSnippets = $snippets.filter(
-    (snippet) =>
-      ($languageFilter == null || snippet.language == $languageFilter) &&
-      ($tagFilters.length == 0 ||
-        $tagFilters.every((fTag) =>
-          snippet.tags.some((tag) => tag.id == fTag.id)
-        ))
+  let search = ''
+
+  const fuseFilter = (snippets: TaggedSnippet[], search: string) =>
+    search != ''
+      ? new Fuse(snippets, {
+          keys: ['title', 'language', 'tags.name', 'code'],
+          includeScore: true,
+          useExtendedSearch: true,
+        })
+          .search(search)
+          .map((res) => ({ ...res, ...res.item }))
+      : snippets
+
+  $: filteredSnippets = fuseFilter(
+    $snippets.filter(
+      (snippet) =>
+        ($languageFilter == null || snippet.language == $languageFilter) &&
+        ($tagFilters.length == 0 ||
+          $tagFilters.every((fTag) =>
+            snippet.tags.some((tag) => tag.id == fTag.id)
+          ))
+    ),
+    search
   )
 </script>
 
@@ -26,7 +44,11 @@
 
   <div class="flex border-r border-r-slate-700 flex-col">
     <div class="p-2 flex gap-2 border-b border-b-slate-700">
-      <input type="search" class="bg-slate-700 rounded px-2" />
+      <input
+        type="search"
+        class="bg-slate-700 rounded px-2"
+        bind:value={search}
+      />
       <button class="bg-slate-700 rounded px-2" on:click={() => createSnippet()}
         >+</button
       >
