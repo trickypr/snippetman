9aa6a2ce0b52193728e36aeb275c09a9224104b5
TrickyPR <23250792+trickypr@users.noreply.github.com>
2023-07-16 20:46:05 +1000

    ♿️ Don't trigger commands when tabbing through

--- src/components/sidebar/SidebarItem.svelte
+++ src/components/sidebar/SidebarItem.svelte
@@ -13,7 +13,9 @@
   aria-selected={selected}
   class="p-1 px-1.5 capitalize hover:bg-slate-700 rounded text-sm cursor-pointer select-none aria-selected:bg-slate-700 aria-selected:hover:bg-slate-600"
   on:click={commandDispatch}
-  on:keydown={commandDispatch}
+  on:keydown={(e) => {
+    if (e.key === 'Enter' || e.key === 'Space') commandDispatch()
+  }}
 >
   <slot />
 </div>
